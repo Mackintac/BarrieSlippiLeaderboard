c18e2e876f2beb600c9a17c286a9c72a0488ce3d
Mackintac <mack-robinson@hotmail.ca>
2025-02-08 13:26:30 -0500

    Remove overwritten SSH_AUTH_SOCK

--- cron/run.sh
+++ cron/run.sh
@@ -4,7 +4,7 @@ export PATH=$HOME/.nvm/versions/node/v20.16.0/bin/node:$HOME/.yarn/bin:$PATH
 export NVM_DIR="$HOME/.nvm"
 source $NVM_DIR/nvm.sh  # Loads nvm so Node.js is available
 
-export SSH_AUTH_SOCK=$(ls /tmp/ssh-*/agent.* | head -n 1)
+# export SSH_AUTH_SOCK=$(ls /tmp/ssh-*/agent.* | head -n 1)
 
 
 # Debugging info
